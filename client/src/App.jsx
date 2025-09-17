1d3291227c44d22799e8a741f81959a35c6abe14
MatiasKV0 <159393104+MatiasKV0@users.noreply.github.com>
2025-09-17 18:48:47 -0300

    add global style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./layout/components/Footer"
+import Header from "./layout/components/Header"
+import './style.css'
 
 function App() {
 
   return (
     <>
+      <Header/>
       <h1>Hola Mundo</h1>
+      <Footer/>
     </>
   )
 }
