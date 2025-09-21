b76d6563ef4b9ad9d8b6decfba69355a16521b10
LukiRote <lukirote123@gmail.com>
2025-09-21 16:48:47 -0300

    feat/api-rest-products-crud

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,21 +1,21 @@
 import productos from "../data/products.js"; 
-
 const getAllProducts = async (req, res) => {
   console.log("Todos los productos:", productos);
   res.json({ productos });
 };
 
-const getProduct = async (req, res) => {
+const getProduct = async (req, res, next) => {
   const id = Number(req.params.id);
   const product = productos.find(p => p.id === id);
 
   if (product) {
     res.json({ product });
   } else {
-    res.status(404).json({ message: "Producto no encontrado" });
+    const error = new Error("Producto no encontrado");
+    error.status = 404;
+    return next(error);
   }
 
   console.log("Producto solicitado:", id);
 };
-
 export { getAllProducts, getProduct };
--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,13 +1,9 @@
 import express from "express";
 
-import {
-  getAllProducts,
-  getProduct
-}from "../controllers/productsController.js"
+import { getAllProducts, getProduct } from "../controllers/productsController.js";
 
 const productsRouter = express.Router();
 
 productsRouter.get("/", getAllProducts);
 productsRouter.get("/:id", getProduct);
-
 export default productsRouter;
