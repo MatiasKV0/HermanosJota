c6a128055ca81428b02f301e4fc2dddac6eb0a7e
MatiasKV0 <159393104+MatiasKV0@users.noreply.github.com>
2025-10-02 20:28:34 -0300

    feat: implement Home page structure with Hero, Destacados, About, and Reviews components

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,8 +1,17 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
+import { useEffect } from "react";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function Layout(){
+
+  const location = useLocation();
+
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[location])
+
   return(
     <>
       <Header/>
